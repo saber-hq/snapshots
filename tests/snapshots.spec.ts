e7ee8d5f151e34b612778be2af9da0362d092d51
Ian Macalinao <ian@saber.so>
2022-03-05 19:06:54 -0600

    fix lintr issues

--- tests/snapshots.spec.ts
+++ tests/snapshots.spec.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-misused-promises */
 import { GokiSDK } from "@gokiprotocol/client";
 import { expectTX, expectTXTable } from "@saberhq/chai-solana";
 import { createMint, sleep, u64 } from "@saberhq/token-utils";
