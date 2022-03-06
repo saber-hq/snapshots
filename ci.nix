4dde3fd8f6e397e7014c2f2281ba82f1a7564f13
Ian Macalinao <ian@saber.so>
2022-03-05 19:08:17 -0600

    fix udev

--- ci.nix
+++ ci.nix
@@ -3,7 +3,7 @@ pkgs.buildEnv {
   name = "ci";
   paths = with pkgs;
     with saber-pkgs;
-    (pkgs.lib.optionals pkgs.stdenv.isLinux ([ libudev ])) ++ [
+    (pkgs.lib.optionals pkgs.stdenv.isLinux ([ udev ])) ++ [
       anchor-0_22_0
       cargo-workspaces
       solana-basic
